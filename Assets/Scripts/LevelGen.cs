8b8af3ed1ac1252617aa0c6540dd823b16af1323
Radin.h <122002544+TheHxin@users.noreply.github.com>
2023-07-13 14:00:26 -0700

    Level Generator BaseCode
    Not functional !!

--- Assets/Scripts/LevelGen.cs
+++ Assets/Scripts/LevelGen.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class LevelGen : MonoBehaviour
@@ -7,5 +8,31 @@ public class LevelGen : MonoBehaviour
     [SerializeField] GameObject Floor;
     [SerializeField] GameObject Wall;
 
+    public float _WallXOffset;
+    public int _PlatformNumber;
+    public int _BarrelNumber;
+    public int[] _PlatformSpace;
 
+    private void Awake()
+    {
+        GenerateBoarder();
+
+        
+        System.Random rn = new System.Random();
+        for (int s = _PlatformNumber; s > 0; s--)
+        {
+            Instantiate(Floor,new Vector3(transform.position.x))
+        }
+    }
+    void GenerateBoarder()
+    {
+        for (int s = _PlatformNumber; s > 0; s--)
+        {
+            Instantiate(Wall, new Vector3((Wall.transform.position.x + _WallXOffset), Wall.transform.position.y + (s * -_PlatformNumber), 0), Wall.transform.rotation);
+        }
+        for (int s = _PlatformNumber; s > 0; s--)
+        {
+            Instantiate(Wall, new Vector3((Wall.transform.position.x - _WallXOffset), Wall.transform.position.y + (s * -_PlatformNumber), 0), Wall.transform.rotation);
+        }
+    }
 }
